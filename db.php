acf36a78400e5b7b6213fbf75e861accc0f047ca
rheypantilo <rpantilo@ssct.edu.ph>
2026-05-01 12:21:27 +0800

    3rd commit

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 $host = getenv("MYSQLHOST");
 $port = getenv("MYSQLPORT");
-$dbname = getenv("MYSQL_DATABASE");
+$dbname = getenv("MYSQLDATABASE"); // ✅ FIXED
 $username = getenv("MYSQLUSER");
 $password = getenv("MYSQLPASSWORD");
 
-$conn = new mysqli($host, $username, $password, $dbname, $port);
+$conn = new mysqli($host, $username, $password, $dbname, (int)$port);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
